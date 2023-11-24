afb36817d3f346284a992ec8c2bfff0ecc5d3bd0
Louis Hagenbucher <louis.hagenbucher@gmail.com>
2023-11-24 16:20:27 +0900

    model pipeline

--- louis_directory/pipe.py
+++ louis_directory/pipe.py
@@ -1,76 +1,81 @@
 
-from models import
 import cv2
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
 
 class Pipe:
 
-    def __init__(self, image):
-        self.image = image
-
+    def __init__(self, file_path=str):
+        self.file_path = file_path
 
     def image_reader(self):
-        if self.image is not None:
-            self.image = cv2.imread(self.image)
+        if self.file_path is not None:
+            self.image_array= cv2.imread(self.file_path)
+            self.image_array = tf.expand_dims(self.image_array, axis=0)
 
     # come back to check which class classifes what
     def get_organ(self):
-        organ_classifier = load_the_classfier_model()
+        self.image_reader()
+        organ_classifier =  load_model('../models/organ_detection_model_final.h5')
         self.organ = None
 
-        if self.image is not None:
-            which_organ = organ_classifier.predict(self.image)
+        if self.image_array is not None:
+            which_organ = organ_classifier.predict(self.image_array)
+            which_organ = np.argmax(which_organ)
 
+            print(which_organ)
             if which_organ == 0:
-                self.organ = 'lung'
+                self.organ = 'knee'
             elif which_organ == 1:
                 self.organ = 'brain'
             elif which_organ == 2:
-                self.organ = 'knee'
-            elif which_organ == 3:
                 self.organ = 'shoulder'
-            elif which_organ == 4:
+            elif which_organ == 3:
                 self.organ = 'spine'
+            elif which_organ == 4:
+                self.organ = 'lung'
 
         return self.organ
 
     def disese_or_not(self):
+        self.organ = self.get_organ()
 
         if self.organ == 'lung':
-            lung_model = LungModelClass() # will be load_model instead
-            disease_model = lung_model()
+            disease_model =  load_model('../models/lung_bin_model.h5') # will be load_model instead
 
         elif self.organ == 'brain':
-            brain_model = BrainModelClass()
-            disease_model = brain_model()
-
+            disease_model= load_model('../models/brain_bin_model.h5')
 
         elif self.organ == 'knee':
-            knee_model = KneeModelClass()
-            disease_model = knee_model()
+            disease_model=  load_model('../models/knee_bin_model.h5')
 
         elif self.organ == 'shoulder':
-            shoulder_model = ShoulderModelClass()
-            disease_model = shoulder_model()
+            disease_model = load_model('../models/shoulder_bin_model.h5')
 
         elif self.organ == 'spine':
-            spine_model = SpineModelClass()
-            disease_model = spine_model()
+            disease_model  = load_model('../models/base_spine_bin_model.h5')
 
-        self.disease = disease_model.predict(self.image)
-        return self.disease
-
+        self.disease = disease_model.predict(self.image_array)[0][0]
+        print(self.disease)
 
     def which_disease(self):
+        self.disese_or_not()
         if self.disease == 1:
             if self.organ == 'lung':
-                return 'lung disease prediction: ' + str(lung_disease_classifier.predict(self.image))
+                lung_cl_model = load_model('../models/lung_class_model.h5')
+                return 'lung disease prediction: ' + str(lung_cl_model.predict(self.image_array))
             elif self.organ == 'brain':
-                return 'brain disease prediction: ' + str(brain_disease_classifier.predict(self.image))
+                brain_cl_model = load_model('../models/brain_class_model.h5')
+                return 'brain disease prediction: ' + str(brain_cl_model.predict(self.image_array))
             elif self.organ == 'knee':
-                return 'knee disease prediction: ' + str(knee_disease_classifier.predict(self.image))
+                knee_cl_model = load_model('../models/knee_class_model.h5')
+                return 'knee disease prediction: ' + str(knee_cl_model.predict(self.image_array))
             elif self.organ == 'shoulder':
-                return 'shoulder disease prediction: ' + str(shoulder_disease_classifier.predict(self.image))
+                shoulder_cl_model = load_model('../models/shoulder_class_model.h5')
+                return 'shoulder disease prediction: ' + str(shoulder_cl_model.predict(self.image_array))
             elif self.organ == 'spine':
-                return 'spine disease prediction: ' + str(spine_disease_classifier.predict(self.image))
+                spine_cl_model = load_model('../models/spine_class_model.h5')
+                return 'spine disease prediction: ' + str(spine_cl_model.predict(self.image_array))
         else:
             return 'is healthy'
