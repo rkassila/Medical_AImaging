df44783d609fef44695fba24aa8214a0a41eff19
waisalkindy <waisalkindy@gmail.com>
2023-11-27 15:06:53 +0900

    try again

--- wais_directory/result.py
+++ wais_directory/result.py
@@ -4,9 +4,11 @@ from PIL import Image
 def app():
     st.title("Analysis result")
 
-    # Display the result stored in session_state
-    if hasattr(st.session_state, "scan_result"):
-        st.write("Scan Result:")
-        st.write(st.session_state.scan_result)
+    # Retrieve the result from session_state
+    result = st.session_state.result
+
+    # Display the result
+    if result is not None:
+        st.success(f"Analysis result: {result}")
     else:
         st.write("Please upload and scan an image first.")
